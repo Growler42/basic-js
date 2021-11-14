aea9471ea16595211b5219042b0addc75217175f
Growler42 <taras_ter@icloud.com>
2021-11-14 15:59:30 +0300

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,19 +1,29 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`);
+    let tempArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next': i += 2; break;
+            case '--discard-prev': tempArr.pop(); break;
+            case '--double-next': if (i + 1 < arr.length) {tempArr.push(arr[i + 1])} break;
+            case '--double-prev': if (i - 1 > 0) {tempArr.push(arr[i - 1])} break;
+            default: tempArr.push(arr[i]);
+        }
+    }
+    return tempArr;
 }
